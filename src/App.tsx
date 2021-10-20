17c3d09d932be8d06452a21d8c5ec8867f57e276
brenthaertlein <brenthaertlein@gmail.com>
2021-10-20 00:31:48 -0400

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>
           spotify-scroll - Scroll spotify
-        </a>
+        </p>
       </header>
     </div>
   );
